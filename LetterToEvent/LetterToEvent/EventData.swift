f624c5deb8baa45545b50c8fd2821bcf95ad879a
EuniceNam <caellime@gmail.com>
2022-08-31 00:15:11 +0900

    feat: 캘린더 연결
    코드 개판임

---
+++ LetterToEvent/LetterToEvent/EventData.swift
@@ -0,0 +1,126 @@
+//
+//  Event+.swift
+//  LetterToEvent
+//
+//  Created by Hyorim Nam on 2022/08/30.
+//
+
+import Foundation
+
+struct EventData {
+    var title: String
+    var startDate: Date?
+    var endDate: Date?
+    var isAllDay: Bool = false
+    var location: String?
+
+    init(text: String) {
+        title = ""
+        let words = text.components(separatedBy: " ")
+        // 월 일 시 분
+        var startDateArray: [Int?] = [nil, nil, nil, nil]
+        var endDateArray: [Int?] = [nil, nil, nil, nil]
+        var isLocationExist = false
+        for word in words {
+            var isWordUsed = false
+            if word.contains(":") {
+                let time = word.split(separator: ":").compactMap{str in Int(str)}
+                if time.count == 2 {
+                    if startDateArray[2] == nil {
+                        startDateArray[2] = time[0]
+                        startDateArray[3] = time[1]
+                        isWordUsed = true
+                    } else if endDateArray[2] == nil {
+                        endDateArray[2] = time[0]
+                        endDateArray[3] = time[1]
+                        isWordUsed = true
+                    }
+                }
+            } else if word.contains(".") {
+                var md = word.split(separator: ".").compactMap{str in Int(str)}
+                if md.count == 2 {
+                    if startDateArray[0] == nil {
+                        startDateArray[0] = md[0]
+                        startDateArray[1] = md[1]
+                        isWordUsed = true
+                    } else if endDateArray[0] == nil {
+                        endDateArray[0] = md[0]
+                        endDateArray[1] = md[1]
+                        isWordUsed = true
+                    }
+                }
+            } else if word.contains("@") {
+                let tmpArr = word.components(separatedBy: "@")
+                if !tmpArr[0].isEmpty {
+                    elseText(tmpArr[0])
+                }
+                isLocationExist = true
+                if !tmpArr[1].isEmpty {
+                    elseText(tmpArr[1])
+                }
+            }
+            if !isWordUsed {
+                elseText(word)
+            }
+        }
+        if title.isEmpty {
+            title = "No title"
+        }
+        
+        if !startDateArray.compactMap{$0}.isEmpty {
+            if startDateArray[3] == nil {
+                isAllDay = true
+            }
+            let startDateComponent = DateComponents(
+                year: Calendar.current.component(.year, from: Date.now),
+                month: startDateArray[0],
+                day: startDateArray[1],
+                hour: startDateArray[2],
+                minute: startDateArray[3])
+            self.startDate = Calendar.current.date(from: startDateComponent)
+        }
+        if !endDateArray.compactMap{$0}.isEmpty {
+            let endDateComponent = DateComponents(
+                year: Calendar.current.component(.year, from: Date.now),
+                month: endDateArray[0],
+                day: endDateArray[1],
+                hour: endDateArray[2],
+                minute: endDateArray[3])
+            self.endDate = Calendar.current.date(from: endDateComponent)
+        }
+        func elseText(_ text: String) {
+            if isLocationExist {
+                if location == nil || location == "" {
+                    location = text
+                } else {
+                    location! += " "
+                    location! += text
+                }
+            } else {
+                if !title.isEmpty {
+                    title += " "
+                } else {
+                    title += text
+                }
+            }
+        }
+    }
+    func isValidEvent() -> Bool {
+        if startDate == nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    func toString() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy년 MM월 dd일 hh시 mm분"
+        let str = "title: \(title)\n"
+            + "start date: \(formatter.string(for: startDate))\n"
+            + "end date: \(formatter.string(for: endDate))\n"
+            + "isAllDay: \(isAllDay)\n"
+            + "location: \(location)\n"
+            + "\n"
+        return str
+    }
+}
--- LetterToEvent/LetterToEvent/MiddleViewController.swift
+++ LetterToEvent/LetterToEvent/MiddleViewController.swift
@@ -10,6 +10,8 @@ import PhotosUI
 import VisionKit
 import Vision
 
+import EventKitUI
+
 class MiddleViewController: UIViewController {
 
     var image: UIImage?
@@ -21,13 +23,63 @@ class MiddleViewController: UIViewController {
     @IBOutlet weak var testImageView: UIImageView!
     @IBOutlet weak var textView: UITextView!
     var transcript = ""
+    var transcriptArray = [String]()
+
+    var events = [EventData]()
+    let eventStore = EKEventStore()
+    @IBOutlet weak var eventPreviewTextView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textLabel.text = "testImage"
+        textLabel.backgroundColor = .green
+        testImageView.image = image
+        testImageView.backgroundColor = .red
+        eventPreviewTextView.text = ""
+
+        self.configTextRecognitionRequest()
+//        DispatchQueue.global(qos: .userInitiated).async {
+            if let image = self.image {
+                self.processImage(image)
+            }
+//        }
+        // 순차실행ㅇ 안 되어 옮김
+//        for i in self.transcriptArray {
+//            print("@in")
+//            self.events.append(EventData(text: i))
+//        }
+//        for ev in self.events {
+//            self.eventPreviewTextView.text += ev.toString()
+//        }
+    }
+
+    @IBAction func addEventAndFinish(_ sender: UIButton) {
+        // auth 확인 후 등록
+        for ev in self.events {
+            if ev.isValidEvent() {
+                if checkEventAuth() {
+                    addEvent(eventdata: ev)
+                    // 캘린더 열기
+                    // 해당 날짜로 열기는 지금 안 함
+                    if let calendarUrl = URL(string: "calshow://") {
+                        if UIApplication.shared.canOpenURL(calendarUrl) {
+                            UIApplication.shared.open(calendarUrl)
+                        }
+                    }
+                    // 홈으로 가기
+                    navigationController?.popToRootViewController(animated: true)
+                }
+            }
+        }
+    }
 
     private func configTextRecognitionRequest() {
         textRecognitionRequest = VNRecognizeTextRequest(completionHandler: { (request, error) in
             if let results = request.results, !results.isEmpty {
                 if let requestResults = request.results as? [VNRecognizedTextObservation] {
                     DispatchQueue.main.async {
-                        print("requestResults: \(requestResults)")
+                        // print("requestResults: \(requestResults)")
                         self.addRecognizedText(recognizedText: requestResults)
                     }
                 }
@@ -51,22 +103,6 @@ class MiddleViewController: UIViewController {
             print("process image handler err: \(error)")
         }
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        textLabel.text = "testImage"
-        textLabel.backgroundColor = .green
-        testImageView.image = image
-        testImageView.backgroundColor = .red
-
-        DispatchQueue.main.async {
-            self.configTextRecognitionRequest()
-            if let image = self.image {
-                self.processImage(image)
-            }
-        }
-    }
 }
 
 extension MiddleViewController {
@@ -76,11 +112,59 @@ extension MiddleViewController {
             guard let candidate = observation.topCandidates(maximumCandidates).first else {
                 continue
             }
+            self.transcriptArray.append(candidate.string)
             transcript += candidate.string
-            print(transcript)
+            // print(transcript)
             // 줄 별로 읽기 시도하기 <- 지금은 모르겠음
             transcript += "\n"
         }
         textView?.text = transcript
+        
+        // viewDidLoad에서 하면 안 됨 (결과가 늦게 와서)
+        for i in self.transcriptArray {
+            self.events.append(EventData(text: i))
+        }
+        for ev in self.events {
+            self.eventPreviewTextView.text += ev.toString()
+        }
+    }
+}
+
+extension MiddleViewController{
+    func checkEventAuth() -> Bool {
+        var auth = false
+        switch EKEventStore.authorizationStatus(for: .event) {
+        case .notDetermined:
+            eventStore.requestAccess(to: .event) { granted, error in
+                if granted {
+                    auth = true
+                }
+            }
+        case .authorized:
+            auth = true
+        default:
+            // alert 띄우기
+            break
+        }
+        return auth
+    }
+    func addEvent(eventdata: EventData) {
+        let event = EKEvent(eventStore: eventStore)
+        // 어느 캘린더를 쓸 지 선택을 추가할 수도 있음
+        event.calendar = eventStore.defaultCalendarForNewEvents
+        event.title = eventdata.title
+        event.startDate = eventdata.startDate
+        event.isAllDay = eventdata.isAllDay
+        if !event.isAllDay && eventdata.endDate == nil {
+            event.endDate = Date.init(timeInterval: 3600, since: event.startDate)
+        } else {
+            event.endDate = eventdata.endDate
+        }
+        event.location = eventdata.location
+        do {
+            try eventStore.save(event, span: .thisEvent, commit: true)
+        } catch {
+            print(error)
+        }
     }
 }
